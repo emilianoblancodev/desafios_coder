bc18f9f9bcc7f1e9e5177596ea4b2f496ff806ca
= <=>
2022-03-02 18:10:43 -0300

    Le doy algo de margen al boton

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -19,7 +19,7 @@ const ItemCount = ({stock, initial, onAdd}) => {
 
   return (
     <>
-    <button  className= "btn btn-warning my-5"disabled={cantidad === 0} onClick={res} >-</button>
+    <button  className= "btn btn-warning mx-5 my-5"disabled={cantidad === 0} onClick={res} >-</button>
     <span className= "mx-3">{cantidad}</span>
     <button  className= "btn btn-warning"disabled={cantidad === stock} onClick={sum} >+</button>
     <button  className= "btn btn-success mx-4"disabled={cantidad === 0} onClick={()=>onAdd(cantidad)} >Agregar al carrito</button>
